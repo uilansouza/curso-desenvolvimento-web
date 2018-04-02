7f9a5f14a182a542aa655ad4dc5ebcca0c91a766
Uilan Souza <uilan.souza@hotmail.com>
2018-04-02 08:36:35 -0300

    jogo da velha

--- Curso JQuery/jogo_da_velha/jogo.js
+++ Curso JQuery/jogo_da_velha/jogo.js
@@ -1,3 +1,23 @@
+var rodada = 1;
+var matriz_jogo = Array(3);
+
+matriz_jogo['a'] = Array(3);
+matriz_jogo['b'] = Array(3);
+matriz_jogo['c'] = Array(3);
+
+matriz_jogo['a'][1] = 0;
+matriz_jogo['a'][2] = 0;
+matriz_jogo['a'][3] = 0;
+
+matriz_jogo['b'][1] = 0;
+matriz_jogo['b'][2] = 0;
+matriz_jogo['b'][3] = 0;
+
+matriz_jogo['c'][1] = 0;
+matriz_jogo['c'][2] = 0;
+matriz_jogo['c'][3] = 0;
+
+
 $(document).ready(function(){
 
 $('#btn_iniciar_jogo').click(function(){
@@ -24,8 +44,105 @@ $('#btn_iniciar_jogo').click(function(){
 
 	$('.jogada').click(function(){
 
-		var id_campo_clicado = this.id
-		alert(id_campo_clicado);
-	})
+		var id_campo_clicado = this.id // recupera o elemento
+		$('#'+id_campo_clicado).off();
+		jogada(id_campo_clicado);
+
+		
+	});
+
+	function jogada(id){
+		var icone ='';
+		var pontos=0;
+
+		if((rodada %2)==1){
+			var icone ='url("imagens/marcacao_1.png")';
+			pontos = -1;
+		}else{
+			var icone ='url("imagens/marcacao_2.png")';
+			pontos = 1;
+		}
+		
+		rodada++;
+
+		$('#'+id).css('background-image',icone);
+
+		var linha_coluna = id.split('-')//trunca(separa) uma cadeia de string e coloca em um array
+		
+		matriz_jogo[linha_coluna[0]][linha_coluna[1]] = pontos;
+
+		console.log(matriz_jogo);
+		verifica_combinacao();
+		
+
+	}
+
+	function verifica_combinacao(){
+		//verifica na horizontal
+		var pontos = 0;
+		//--------------VERIFICAR A ---------- //
+
+		for(var i =1 ; i<=3; i++){
+			pontos = pontos+matriz_jogo['a'][i];
+
+
+		}
+		ganhador(pontos);
+
+		//--------------VERIFICAR B ---------- //
+
+		pontos = 0	;
+		for(var i =1 ; i<=3; i++){
+			pontos = pontos+matriz_jogo['b'][i];
+
+
+		}
+		ganhador(pontos);
+
+		//--------------VERIFICAR C ---------- //
+		pontos =0;
+
+		for(var i =1 ; i<=3; i++){
+			pontos = pontos+matriz_jogo['c'][i];
+
+
+		}
+		ganhador(pontos);
+
+		//********* VERIFICA NA VERTICAL ********/
+		
+		for( var l=1 ; l<=3; l++){
+			 pontos =0;
+
+			pontos +=matriz_jogo['a'][l];		
+			pontos +=matriz_jogo['b'][l];		
+			pontos +=matriz_jogo['c'][l];
+			ganhador(pontos);		
+
+		}
+
+		//********* VERIFICA NA DIAGONAL ********/
+		pontos = 0;
+		pontos = matriz_jogo['a'][1]+matriz_jogo['b'][2]+matriz_jogo['c'][3];
+		ganhador(pontos);
+
+		pontos = 0;
+		pontos = matriz_jogo['a'][3]+matriz_jogo['b'][2]+matriz_jogo['c'][1];
+		ganhador(pontos);
+
+	}
+
+	function ganhador(pontos){
+		if(pontos==-3){
+			var jogada_1 = $('#entrada_apelido_jogador_1').val();
+			alert(jogada_1+' é o vencedor');
+			$('.jogada').off();
+
+		}else if(pontos==3){
+			var jogada_2 = $('#entrada_apelido_jogador_2').val();
+			alert(jogada_2+' é o vencedor');
+			$('.jogada').off();
+		}
+	}
 
 });
